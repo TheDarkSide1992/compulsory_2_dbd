4d4ad6a77c7086be2f199e09efbbdbaed427bf6e
TheDarkSide1992 <Jenscp171@gmail.com>
2025-05-05 10:09:54 +0200

    Init

---
+++ ECommerce/Controllers/ECommerceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ECommerceController : ControllerBase
+{
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<IActionResult> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+﻿# Compulsory_2_dbd
+This is a school project at EASV purely based on architectural design for an e-commerce application.
+## by
+* Jens
+* Emil
+* Andreas
